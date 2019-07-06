60531896fc2659107e66c4138c9a052696e5fcfd
iDea2016su <hk.nesc@gmail.com>
2019-07-06 16:04:02 +0800

    update

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+
+#include "RangingCorrection.h"
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+
+	cout<<"system start"<<endl;
+	int SpreadingFactor = 0x50;
+	int BandWidth = 0x0A;
+	double media = 10.0;
+
+	double dis = sx1280RangingCorrection::ComputeRangingCorrectionPolynome(SpreadingFactor,BandWidth,media);
+    
+    cout<<"dis is"<<dis<<endl;
+    cout<<"system finish"<<endl;
+	return 0;
+}
